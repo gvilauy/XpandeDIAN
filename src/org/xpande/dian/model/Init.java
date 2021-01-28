cee7133533515f22f209618fa16a55bfb792aeaf
gabriel <gabrielvila13@gmail.com>
2021-01-28 09:28:10 -0300

    Initial commit

---
+++ src/org/xpande/dian/model/Init.java
@@ -0,0 +1,8 @@
+package org.xpande.dian.model;
+
+/**
+ * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
+ * Xpande. Created by Gabriel Vila on 1/28/21.
+ */
+public class Init {
+}
